c3c20602cc6695042cde40492dd7d8068b7d3e18
kothru <1894683+kothru@users.noreply.github.com>
2022-01-31 08:58:50 +0900

    add input proc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-const sayHello = (name: string) => {
-  return `Hello, ${name}!`
-}
+import { resolve } from "path/posix"
 
-process.stdout.write(sayHello('abc'))
+const printLine = (text: string, breakLine: boolean = true) => {
+  process.stdout.write(text + (breakLine ? '\n' : ''))
+}
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n> `, false)
+  const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
+  return input.trim()
+}
+  ; (async () => {
+    const name = await promptInput('input name...')
+    console.log(name)
+    const age = await promptInput('input age...')
+    console.log(age)
+    process.exit()
+  })()
