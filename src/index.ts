f29533193ce0ead6480f63cc2aeb90c5f1a71d72
kothru <1894683+kothru@users.noreply.github.com>
2022-02-01 08:44:53 +0900

    remove constructer

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,9 @@ const promptInput = async (text: string) => {
   return input.trim()
 }
 class HitAndBlow {
-  answerSource: string[]
-  answer: string[]
-  tryCount: number
-
-  constructor() {
-    this.answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    this.answer = []
-    this.tryCount = 0
-  }
+  answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  answer: string[] = []
+  tryCount = 0
 }
 ; (async () => {
   const hitAndBlow = new HitAndBlow()
