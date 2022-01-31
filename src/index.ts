d3c53d6b16141af9299071fdbe593623b3164150
kothru <1894683+kothru@users.noreply.github.com>
2022-02-01 08:52:50 +0900

    add setting

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,18 @@ class HitAndBlow {
   answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   answer: string[] = []
   tryCount = 0
+  setting() {
+    const answerLength = 3
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length)
+      const selectedItem = this.answerSource[randNum]
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem)
+      }
+    }
+  }
 }
 ; (async () => {
   const hitAndBlow = new HitAndBlow()
+  hitAndBlow.setting()
 })()
