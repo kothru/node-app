d07b730dbf054749fa115402d37939cd39cefd94
kothru <1894683+kothru@users.noreply.github.com>
2022-02-01 08:43:43 +0900

    add class

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,17 @@ const promptInput = async (text: string) => {
   const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
   return input.trim()
 }
-  ; (async () => {
-    const name = await promptInput('input name...')
-    console.log(name)
-    const age = await promptInput('input age...')
-    console.log(age)
-    process.exit()
-  })()
+class HitAndBlow {
+  answerSource: string[]
+  answer: string[]
+  tryCount: number
+
+  constructor() {
+    this.answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    this.answer = []
+    this.tryCount = 0
+  }
+}
+; (async () => {
+  const hitAndBlow = new HitAndBlow()
+})()
