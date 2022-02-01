6ae1a03b3a21447e3676f02065ac1e1556a960d3
kothru <1894683+kothru@users.noreply.github.com>
2022-02-01 09:01:06 +0900

    add comment

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
+/**
+ * print one line
+ * @param text print text
+ * @param breakLine print break line if true
+ */
 const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + (breakLine ? '\n' : ''))
 }
+/**
+ * prompt stdin input
+ * @param text print prompt text
+ * @returns input string
+ */
 const promptInput = async (text: string) => {
   printLine(`\n${text}\n> `, false)
   const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
   return input.trim()
 }
+/**
+ * game class
+ */
 class HitAndBlow {
+  /**
+   * answer item source
+   */
   answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  /**
+   * set answer item
+   */
   answer: string[] = []
+  /**
+   * game try count
+   */
   tryCount = 0
+  /**
+   * initialize game
+   */
   setting() {
     const answerLength = 3
     while (this.answer.length < answerLength) {
@@ -20,10 +45,17 @@ class HitAndBlow {
       }
     }
   }
+  /**
+   * playing game
+   */
   async play() {
     const inputArr = (await promptInput('input 3 num separate ,')).split(',')
+
   }
 }
+/**
+ * IIFE
+ */
 ; (async () => {
   const hitAndBlow = new HitAndBlow()
   hitAndBlow.setting()
